0ce80a751627e28a21690c4480bb7c8543fe6d5a
ThinhLD <leduythinh20001@gmail.com>
2024-08-01 08:14:19 +0700

    update

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,10 +1,49 @@
 package com.example;
 
-import com.example.manager.ConsoleManager;
+import com.example.manager.CategoryManager;
+import com.example.manager.ConversionManager;
+import com.example.manager.GeographicManager;
+import com.example.manager.UserManager;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ConsoleManager consoleManager = new ConsoleManager();
-        consoleManager.start();
+        Scanner scanner = new Scanner(System.in);
+        UserManager userManager = new UserManager();
+
+        userManager.manageLogin(scanner);
+
+        boolean running = true;
+        while (running) {
+            System.out.println("Main Menu:");
+            System.out.println("1. Category Management");
+            System.out.println("2. Geographic Area Management");
+            System.out.println("3. Conversion Factor Management");
+            System.out.println("4. Exit");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // consume newline
+
+            switch (choice) {
+                case 1:
+                    CategoryManager categoryManager = new CategoryManager("categories.xml");
+                    categoryManager.manage(scanner);
+                    break;
+                case 2:
+                    GeographicManager geographicManager = new GeographicManager("geographicAreas.xml");
+                    geographicManager.manage(scanner);
+                    break;
+                case 3:
+                    ConversionManager conversionManager = new ConversionManager("conversionFactors.xml");
+                    conversionManager.manage(scanner);
+                    break;
+                case 4:
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Try again.");
+            }
+        }
     }
 }
--- src/main/java/com/example/util/XMLManager.java
+++ src/main/java/com/example/util/XMLManager.java
@@ -2,9 +2,10 @@ package com.example.util;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -19,16 +20,26 @@ public class XMLManager {
         return builder.parse(new File(filePath));
     }
 
+    public static Document createDocument() throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        return builder.newDocument();
+    }
+
     public static void saveXML(Document doc, String filePath) throws Exception {
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
         DOMSource source = new DOMSource(doc);
         StreamResult result = new StreamResult(new File(filePath));
         transformer.transform(source, result);
     }
 
     public static Element getElementByTagName(Document doc, String tagName) {
-        NodeList nodeList = doc.getElementsByTagName(tagName);
-        return (Element) nodeList.item(0);
+        return (Element) doc.getElementsByTagName(tagName).item(0);
+    }
+
+    public static boolean fileExists(String filePath) {
+        return new File(filePath).exists();
     }
 }

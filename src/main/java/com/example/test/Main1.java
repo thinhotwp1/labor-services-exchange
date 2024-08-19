10fd5f8fc3f0010dbda000bedce922b15c3a00c9
HIEULD <hieu45605@gmail.com>
2024-08-19 21:37:30 +0700

    finish 19/8/2024

--- src/main/java/com/example/test/Main1.java
+++ src/main/java/com/example/test/Main1.java
@@ -1,27 +1,37 @@
 package com.example.test;
 
+import java.util.SortedMap;
+
 public class Main1 {
     public static void main(String[] args) {
 
         // Tạo entity con người, các thộc tính: (name, age, gender, address, number)
         // gán du lieu va in ra cac thuoc tinh cua con nguoi do
-        Person thinh = new Person();
-        thinh.setName("Thinh");
-        thinh.setAge(23);
-        thinh.setGender("Nam");
-        thinh.setAddress("123 ABC, TP. Ha Noi");
-        thinh.setPhoneNumber("0923001270");
-        thinh.setMoneyAmount(200.5);
-
-        // sout + tab
-        System.out.println(thinh.getName());
-        System.out.println(thinh.getAge());
+//        Person thinh = new Person();
+//        thinh.setName("Thinh");
+//        thinh.setAge(23);
+//        thinh.setGender("Nam");
+//        thinh.setAddress("123 ABC, TP. Ha Noi");
+//        thinh.setPhoneNumber("0923001270");
+//        thinh.setMoneyAmount(200.5);
+//
+//        // sout + tab
+//        System.out.println(thinh.getName());
+//        System.out.println(thinh.getAge());
 
         // Tao entity man hinh: inch, hangSanXuat, ngaySanXuat, giaTien, canNang
         // In ra man hinh cac gia tri
-
-
-
-
+        ManHinh manHinhLG = new ManHinh();
+        manHinhLG.setInch(24);
+        manHinhLG.setCanNang(3);
+        manHinhLG.setGiaTien(1700);
+        manHinhLG.setHangSanXuat("LG");
+        manHinhLG.setNgaySanXuat("20.1.2024");
+        System.out.println(manHinhLG.getCanNang());
+        System.out.println(manHinhLG.getGiaTien());
+        System.out.println(manHinhLG.getHangSanXuat());
+        System.out.println(manHinhLG.getNgaySanXuat());
+        System.out.println(manHinhLG.getInch());
     }
+
 }
---
+++ src/main/java/com/example/test/ManHinh.java
@@ -0,0 +1,52 @@
+package com.example.test;
+
+public class ManHinh {
+    private double inch;
+    private String hangSanXuat;
+    private  String ngaySanXuat;
+    private double giaTien;
+    private  int canNang;
+//2.contructor
+    public ManHinh() {
+    }
+
+    public double getInch() {
+        return inch;
+    }
+
+    public void setInch(double inch) {
+        this.inch = inch;
+    }
+
+    public String getHangSanXuat() {
+        return hangSanXuat;
+    }
+
+    public void setHangSanXuat(String hangSanXuat) {
+        this.hangSanXuat = hangSanXuat;
+    }
+
+    public String getNgaySanXuat() {
+        return ngaySanXuat;
+    }
+
+    public void setNgaySanXuat(String ngaySanXuat) {
+        this.ngaySanXuat = ngaySanXuat;
+    }
+
+    public double getGiaTien() {
+        return giaTien;
+    }
+
+    public void setGiaTien(double giaTien) {
+        this.giaTien = giaTien;
+    }
+
+    public int getCanNang() {
+        return canNang;
+    }
+
+    public void setCanNang(int canNang) {
+        this.canNang = canNang;
+    }
+}

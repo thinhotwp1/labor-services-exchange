4dbc2003c8bb0c92fd42266aea3259eb323aa8af
ThinhLD <leduythinh20001@gmail.com>
2024-08-19 21:06:41 +0700

    update test

---
+++ src/main/java/com/example/test/Main1.java
@@ -0,0 +1,27 @@
+package com.example.test;
+
+public class Main1 {
+    public static void main(String[] args) {
+
+        // Tạo entity con người, các thộc tính: (name, age, gender, address, number)
+        // gán du lieu va in ra cac thuoc tinh cua con nguoi do
+        Person thinh = new Person();
+        thinh.setName("Thinh");
+        thinh.setAge(23);
+        thinh.setGender("Nam");
+        thinh.setAddress("123 ABC, TP. HCM");
+        thinh.setPhoneNumber("0923001270");
+        thinh.setMoneyAmount(200.5);
+
+        // sout + tab
+        System.out.println(thinh.getName());
+        System.out.println(thinh.getAge());
+
+        // Tao entity man hinh: inch, hangSanXuat, ngaySanXuat, giaTien, canNang
+        // In ra man hinh cac gia tri
+
+
+
+
+    }
+}
--- src/main/java/com/example/test/Person.java
+++ src/main/java/com/example/test/Person.java
@@ -1,2 +1,64 @@
-package com.example.test;public class Person {
+package com.example.test;
+
+public class Person {
+    // 1 Thuoc tinh
+    private String name;
+    private int age; // so nguyen
+    private String address;
+    private String gender;
+    private String phoneNumber; // 09123456789, 0123456789
+    private double moneyAmount; // 12.5, 20.2
+
+    // 2. Constructor
+    public Person() {
+    }
+
+    // 3. Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public double getMoneyAmount() {
+        return moneyAmount;
+    }
+
+    public void setMoneyAmount(double moneyAmount) {
+        this.moneyAmount = moneyAmount;
+    }
 }
